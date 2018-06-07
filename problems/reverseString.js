6e25b3c89d1e97ff24cc0c042c1dadc301578fe0
tjshiu <tiffany.shiu.2@gmail.com>
2018-06-06 18:19:50 -0700

    utilized a switch

--- problems/reverseString.js
+++ problems/reverseString.js
@@ -11,10 +11,9 @@ Given s = "hello", return "olleh".
 const reverseString = function(s) {
     let chars = s.split("");
     for (let i = 0; i < Math.floor(chars.length / 2); i++) {
-        let temp = chars[i];
-        chars[i] = chars[chars.length - 1 - i];
-        chars[chars.length - 1 - i] = temp;
+        [chars[i], chars[chars.length - 1 - i]] = [chars[chars.length - 1 - i], chars[i]];
     }
+
     return chars.join("");
 };
 
@@ -23,6 +22,4 @@ One liner
 const reverseString = function(s) {
   return s.split("").reverse().join("");
 }
-
-Also JavaScript doesn't have an easy swap method. Need to swap with a temp.
 */
