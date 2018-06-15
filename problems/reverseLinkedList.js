705694280ebb8d034d82d506ab12c50447845024
tjshiu <tiffany.shiu.2@gmail.com>
2018-06-15 14:47:06 -0700

    added recursive solution for reverse linked list

--- problems/reverseLinkedList.js
+++ problems/reverseLinkedList.js
@@ -10,30 +10,53 @@ Follow up:
 A linked list can be reversed either iteratively or recursively. Could you implement both?
 */
 
-/**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
+// /**
+//  * Definition for singly-linked list.
+//  * function ListNode(val) {
+//  *     this.val = val;
+//  *     this.next = null;
+//  * }
+//  */
+// /**
+//  * @param {ListNode} head
+//  * @return {ListNode}
+//  */
+// var reverseList = function(head) {
+//     if (!head) return [];
+//
+//     let next = head.next;
+//     let current = head;
+//     current.next = null;
+//
+//     while (next !== null) {
+//         let newNext = next.next;
+//         next.next = current;
+//         current = next;
+//         next = newNext;
+//     }
+//
+//     return current;
+// };
+
 var reverseList = function(head) {
-    if (!head) return [];
-    
-    let next = head.next;
-    let current = head;
-    current.next = null;
+  let prev = null;
+  let curr = head;
 
-    while (next !== null) {
-        let newNext = next.next;
-        next.next = current;
-        current = next;
-        next = newNext;
-    }
+  while (curr !== null) {
+    let nextTemp = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = nextTemp;
+  }
 
-    return current;
+  return prev;
+};
+
+var reverseListRecursive = function(head) {
+  if (head === null || head.next === null) return head;
+
+  let p = reverseList(head.next);
+  head.next.next = head;
+  head.next = null;
+  return p;
 };
