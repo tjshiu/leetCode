6d5bee0cdff2cd46ff7713d02b76ad4e95c6ef40
tjshiu <tiffany.shiu.2@gmail.com>
2018-06-23 20:31:18 -0700

    Invert a binary tree

---
+++ problems/invertBinaryTree.js
@@ -0,0 +1,45 @@
+/* Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if (!root) return null;
+
+    let oldLeft = root.left;
+    let oldRight = root.right;
+
+    root.right = invertTree(oldLeft);
+    root.left = invertTree(oldRight);
+
+    return root;
+};
